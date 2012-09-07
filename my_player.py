d06f64de0cea1916e9327c6a3060e74d72602d05
Your Name <you@example.com>
2012-09-07 14:14:46 +0200

    Add class OurDefensePlayer

--- my_player.py
+++ my_player.py
@@ -190,3 +190,142 @@ class IdentityCrisis(AbstractPlayer):
 
         return diff_pos(self.current_pos, food_path.pop())
 
+
+class OurDefensePlayer(AbstractPlayer):
+    """ A crude defensive player.
+
+    Will move towards the border, and as soon as it notices enemies in its
+    territory, it will start to track them. When it kills the enemy it returns
+    to the border and waits there for more. Like the BFSPlayer this player
+    stores the maze as an adjacency list [1] but uses the breadth first search [2] to
+    find the closest position on the border.  However it additionally uses the
+    A* (A Star) search [3] to find the shortest path to its target.
+
+    The adjacency lits representation (AdjacencyList) and A* search
+    (AdjacencyList.a_star) are imported from pelita.graph.
+
+    * [1] http://en.wikipedia.org/wiki/Adjacency_list
+    * [2] http://en.wikipedia.org/wiki/Breadth-first_search
+    * [3] http://en.wikipedia.org/wiki/A*_search_algorithm
+
+    """
+    def set_initial(self):
+        self.adjacency = AdjacencyList(self.current_uni)
+        self.path = self.path_to_border
+        self.tracking_idx = None
+
+    @property
+    def path_to_border(self):
+        """ Path to the closest border position. """
+        try:
+            return self.adjacency.bfs(self.current_pos, self.group_consecutives(self.team_border))
+            #return self.adjacency.bfs(self.current_pos, self.team_border)
+        except NoPathException:
+            return None
+    
+    #@property
+    def evaluate_borders(self, positions):
+        good_borders = []
+        #print positions
+        ns = set([(0, -1), (0, 1)])
+        #print "ns is", ns
+        #ns.isdisjoint(
+        #moves = set(self.legal_moves.keys())
+        #print "keys are", moves
+        #if ns.isdisjoint(moves):
+        print self.group_consecutives(positions)
+        
+        
+        for x,y in positions:
+            p_north = (x,y+1) in positions 
+            p_south = (x,y-1) in positions
+            if p_north or p_south:
+                good_borders.append((x,y))
+                  
+        if len(good_borders) == 0:
+            good_borders = positions 
+        return good_borders
+    
+    def group_consecutives(self,border_positions):
+        try:
+            import operator,itertools
+        except:
+            return border_positions
+        list_consecutives = []
+        y_coordinates = [p[1] for p in border_positions]
+        x = border_positions[0][0]
+        for k, g in itertools.groupby(enumerate(y_coordinates), lambda (i,x):i-x):
+            list_consecutives.append([(x,y) for y in map(operator.itemgetter(1), g)])
+        
+        return sorted(list_consecutives,key=len).pop()
+                
+    
+    @property
+    def path_to_target(self):
+        """ Path to the target we are currently tracking. """
+        try:
+            return self.adjacency.a_star(self.current_pos,
+                    self.tracking_target.current_pos)
+        except NoPathException:
+            return None
+
+    @property
+    def tracking_target(self):
+        """ Bot object we are currently tracking. """
+        self.say("come here! Dont run away you chicken")
+        return self.current_uni.bots[self.tracking_idx]
+
+    #def get_move_defense(self):
+    def get_move(self):
+        
+        # if we were killed, for whatever reason, reset the path
+        if self.current_pos == self.initial_pos or self.path is None:
+            self.path = self.path_to_border
+
+        # First we need to check, if our tracked enemy is still
+        # in our zone
+        if self.tracking_idx is not None:
+            # if the enemy is no longer in our zone
+            if not self.team.in_zone(self.tracking_target.current_pos):
+                self.tracking_idx = None
+                self.path = self.path_to_border
+            # otherwise update the path to the target
+            else:
+                self.path = self.path_to_target
+
+        # if we are not currently tracking anything
+        # (need to check explicity for None, because using 'if 
+        # self.tracking_idx' would evaluate to True also when we are tracking
+        # the bot with index == 0)
+        if self.tracking_idx is None:
+            # check the enemy positions
+            possible_targets = [enemy for enemy in self.enemy_bots
+                    if self.team.in_zone(enemy.current_pos)]
+            if possible_targets:
+                # get the path to the closest one
+                try:
+                    possible_paths = [(enemy, self.adjacency.a_star(self.current_pos, enemy.current_pos))
+                                      for enemy in possible_targets]
+                except NoPathException:
+                    possible_paths = []
+            else:
+                possible_paths = []
+
+            if possible_paths:
+                closest_enemy, path = min(possible_paths,
+                                          key=lambda enemy_path: len(enemy_path[1]))
+
+                # track that bot by using its index
+                self.tracking_idx = closest_enemy.index
+                self.path = path
+            else:
+                # otherwise keep going if we aren't already underway
+                if not self.path:
+                    self.path = self.path_to_border
+
+        # if something above went wrong, just stand still
+        if not self.path:
+            return datamodel.stop
+        else:
+            return diff_pos(self.current_pos, self.path.pop())
+
